9d13af33bd7fe8a67e0b2b00ae9501cb2485d3f2
Tomas Sychra <xsychr03@gmail.com>
2012-12-05 16:32:00 +0100

    new class Gui created

---
+++ src/xroads/gui/BasicGui.java
@@ -0,0 +1,16 @@
+package xroads.gui;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+@SuppressWarnings("serial")
+public class BasicGui extends JFrame {
+	public BasicGui() {
+		setTitle("XRoads");
+		setSize(800, 600);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);		
+	}
+		
+
+}
