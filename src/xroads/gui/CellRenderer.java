bf3e48be400e5955cf9251cd5d86180bc52b1213
Tomas Sychra <xsychr03@gmail.com>
2012-12-06 17:42:22 +0100

    gui changing color on value input

--- src/xroads/gui/CellRenderer.java
+++ src/xroads/gui/CellRenderer.java
@@ -11,7 +11,7 @@ import javax.swing.table.TableCellRenderer;
 @SuppressWarnings("serial")
 public class CellRenderer extends DefaultTableCellRenderer {
 	private DefaultTableModel model;
-
+	private float max = 30f; 
 	
 	/**
 	 * Associate renderer with table model
@@ -26,18 +26,54 @@ public class CellRenderer extends DefaultTableCellRenderer {
 			boolean isSelected, boolean hasFocus, int row, int column) {
 			 
 		//System.out.println("Cell: " + Integer.toString(row) + " : " + Integer.toString(column));
-		System.out.println("Row: " + row);
+		//System.out.println("Row: " + row);
+		
 		if(row % 3 == 0 && column % 3 == 0) {
 			//System.out.println("Row formated: " + row);
 			setBackground(Color.WHITE);
 		} else if ((row % 3 == 0 && column % 3 != 0) || (row % 3 != 0 && column % 3 == 0)) {
 			// TODO set sth, what will change color while value is changing
-			setBackground(Color.GREEN);
+			if(value == null) {
+				setBackground(Color.GREEN);
+			} else {
+				setBackground(interpolateColorFromValue(Integer.parseInt((String)value)));
+			}
+			
 		} else if (row % 3 != 0 && column % 3 != 0){
 			setBackground(Color.GRAY);
 		}
 	
 		return this;
+	}
+
+	/**
+	 * Interpolate color from value 0 - MAX
+	 * @param parseInt
+	 * @return
+	 */
+	private Color interpolateColorFromValue(int cars) {
+		float carsf = cars / max;
+		
+		// Green color channel
+		float origGreenRChannel = Color.GREEN.getRed() / 255f;
+		float origGreenGChannel = Color.GREEN.getGreen() / 255f;
+		float origGreenBChannel = Color.GREEN.getBlue() / 255f;
+		float origGreenAChannel = Color.GREEN.getAlpha() / 255f;
+		
+		// Red color channel
+		float origRedRChannel = Color.RED.getRed() / 255f;
+		float origRedGChannel = Color.RED.getGreen() / 255f;
+		float origRedBChannel = Color.RED.getBlue() / 255f;
+		float origRedAChannel = Color.RED.getAlpha() / 255f;
+		
+		// Interpolate color
+		float red =  origRedRChannel * carsf + origGreenRChannel * (1f - carsf);
+		float green =  origRedGChannel * carsf + origGreenGChannel * (1f - carsf);
+		float blue =  origRedBChannel * carsf + origGreenBChannel * (1f - carsf);
+		float alpha =  origRedAChannel * carsf + origGreenAChannel * (1f - carsf);
+		
+
+		return new Color(red, green, blue, alpha);	
 	}
 	
 	
--- src/xroads/gui/XroadsGui.java
+++ src/xroads/gui/XroadsGui.java
@@ -20,6 +20,7 @@ import javax.swing.UIManager;
 import javax.swing.ListSelectionModel;
 import java.awt.Color;
 import javax.swing.border.LineBorder;
+import javax.swing.table.DefaultTableModel;
 
 public class XroadsGui extends JFrame {
 
@@ -27,6 +28,14 @@ public class XroadsGui extends JFrame {
 	private JTextField widthDimens;
 	private JTextField heightDimens;
 	private CityGenerator city;
+	private JTable table;
+	private JTable table_1;
+	private JTable table_2;
+	private JTable table_3;
+	private JTable table_4;
+	private JTable table_5;
+	private JTable table_6;
+	private JTable table_7;
 
 	/**
 	 * Create the frame.
@@ -97,6 +106,64 @@ public class XroadsGui extends JFrame {
 		city.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		city.setBounds(5, 109, 751, 426);
 		contentPane.add(city);
+		
+		table = new JTable();
+		table.setBackground(Color.GRAY);
+		table.setBounds(577, 29, 22, 22);
+		contentPane.add(table);
+		
+		table_1 = new JTable();
+		table_1.setBackground(Color.GRAY);
+		table_1.setBounds(599, 29, 22, 22);
+		contentPane.add(table_1);
+		
+		table_2 = new JTable();
+		table_2.setBackground(Color.GRAY);
+		table_2.setBounds(599, 50, 22, 22);
+		contentPane.add(table_2);
+		
+		table_3 = new JTable();
+		table_3.setBackground(Color.GRAY);
+		table_3.setBounds(577, 50, 22, 22);
+		contentPane.add(table_3);
+		
+		table_4 = new JTable();
+		table_4.setBackground(Color.RED);
+		table_4.setBounds(623, 29, 22, 22);
+		contentPane.add(table_4);
+		
+		table_5 = new JTable();
+		table_5.setModel(new DefaultTableModel(
+			new Object[][] {
+			},
+			new String[] {
+			}
+		));
+		table_5.setBackground(Color.ORANGE);
+		table_5.setBounds(577, 5, 22, 22);
+		contentPane.add(table_5);
+		
+		table_6 = new JTable();
+		table_6.setBackground(Color.ORANGE);
+		table_6.setBounds(553, 50, 22, 22);
+		contentPane.add(table_6);
+		
+		table_7 = new JTable();
+		table_7.setBackground(Color.GREEN);
+		table_7.setBounds(599, 74, 22, 22);
+		contentPane.add(table_7);
+		
+		JLabel lblLegend = new JLabel("Legend of crossroad :");
+		lblLegend.setBounds(413, 5, 132, 14);
+		contentPane.add(lblLegend);
+		
+		JLabel lblStuck = new JLabel("Stuck road");
+		lblStuck.setBounds(655, 29, 70, 14);
+		contentPane.add(lblStuck);
+		
+		JLabel lblFree = new JLabel("Free road");
+		lblFree.setBounds(631, 74, 70, 14);
+		contentPane.add(lblFree);
 		
 		
 		this.setVisible(true);
