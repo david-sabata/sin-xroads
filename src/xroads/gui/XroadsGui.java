7a0f76865938a3fa67d1fc3380d5c8475333e06f
Tomas Sychra <xsychr03@gmail.com>
2012-12-06 22:24:51 +0100

    default matrix 5x5

--- src/xroads/gui/XroadsGui.java
+++ src/xroads/gui/XroadsGui.java
@@ -84,11 +84,13 @@ public class XroadsGui extends JFrame {
 		panel.add(lblWidth);
 		
 		widthDimens = new JTextField();
+		widthDimens.setText("5");
 		widthDimens.setBounds(85, 31, 86, 20);
 		panel.add(widthDimens);
 		widthDimens.setColumns(10);
 		
 		heightDimens = new JTextField();
+		heightDimens.setText("5");
 		heightDimens.setBounds(85, 60, 86, 20);
 		panel.add(heightDimens);
 		heightDimens.setColumns(10);
