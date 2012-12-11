26c006e536f56898251a52e67cd9536933507e81
david-sabata <david.n.sabata@gmail.com>
2012-12-11 20:09:16 +0100

    skripty

--- compile.sh
+++ compile.sh
@@ -3,7 +3,7 @@
 mkdir -p build
 chmod a+xr build
 
-javac -sourcepath src -d build -encoding UTF-8 -classpath libs/jade.jar:libs/commons-codec-1.3.jar src/xroads/*.java
+#javac -sourcepath src -d build -encoding UTF-8 -classpath libs/jade.jar:libs/commons-codec-1.3.jar src/xroads/*.java
 javac -sourcepath src -d build -encoding UTF-8 -classpath libs/jade.jar:libs/commons-codec-1.3.jar src/xroads/agents/*.java
 javac -sourcepath src -d build -encoding UTF-8 -classpath libs/jade.jar:libs/commons-codec-1.3.jar src/xroads/behaviours/*.java
 javac -sourcepath src -d build -encoding UTF-8 -classpath libs/jade.jar:libs/commons-codec-1.3.jar src/xroads/behaviours/carfsm/*.java
@@ -15,4 +15,4 @@ mkdir -p bin
 cp libs/jade.jar bin/jade.jar
 cp libs/commons-codec-1.3.jar bin/commons-codec-1.3.jar
 
-jar cf bin/sin-project.jar build/xroads/*.class build/xroads/agents/*.class build/xroads/behaviours/*.class build/xroads/behaviours/carfsm/*.class build/xroads/behaviours/xroadfsm/*.class build/xroads/gui/*.class
+jar cmf src/manifest bin/sin-project.jar build/xroads/*.class build/xroads/agents/*.class build/xroads/behaviours/*.class build/xroads/behaviours/carfsm/*.class build/xroads/behaviours/xroadfsm/*.class build/xroads/gui/*.class
---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mkdir -p libs
+cd libs
+
+wget www.stud.fit.vutbr.cz/~xsabat01/sin/jade.jar
+wget www.stud.fit.vutbr.cz/~xsabat01/sin/commons-codec-1.3.jar
+
+cd ..
